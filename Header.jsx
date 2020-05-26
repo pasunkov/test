aafc3e672626bbd3682be06b76d0afbcc746c716
pasunkov <a.pasunkov@gmail.com>
2020-05-26 23:16:51 +0300

    changes width

--- Header.jsx
+++ Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return <header className="header">
         <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
-            width={2000} alt=""/>
+            width={100} alt=""/>
     </header>
 }
 
