3a2c87d0053076a740acc672d96eb46df54235db
pasunkov <a.pasunkov@gmail.com>
2020-05-26 23:15:08 +0300

    changes width

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
+            width={2000} alt=""/>
+    </header>
+}
+
+export default Header;
